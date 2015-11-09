6dbb556e05195ca813e6743df466652b7a577d10
Harry Oh <harry.oh@udptechnology.com>
2015-11-10 03:46:34 +0900

    Version 0.1.5

--- server/api/history/history.controller.js
+++ server/api/history/history.controller.js
@@ -61,28 +61,146 @@
     });
   };
 
-
-  /*  MONGO Query
-  db.getCollection('urls').aggregate([
-      {
-          $match: {
-              createdAt: {
-                  $gte: ISODate("2015-11-01T00:00:00.000Z"),
-                  $lt: ISODate("2015-11-16T00:00:00.000Z")
-              }
-          }
-      },
-      {
-          $group: {
-          _id: {
-              year: {$year: "$createdAt"},
-              month: {$month: "$createdAt"},
-              dayOfMonth: {$dayOfMonth: "$createdAt"}
-          },
-          count: {$sum: 1}
+  exports.browser = function(req, res) {
+    return Logger.count(function(err, length) {
+      if (err) {
+        return handleError(res, err);
       }
-  }])
-   */
+      if (!length) {
+        return res.status(200).end();
+      }
+      return Logger.aggregate([
+        {
+          $group: {
+            _id: {
+              browser: '$browser',
+              id: '$_id'
+            }
+          }
+        }, {
+          $group: {
+            _id: '$_id.browser',
+            count: {
+              $sum: 1
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            browser: '$_id',
+            count: 1
+          }
+        }
+      ], function(err, result) {
+        var data;
+        if (err) {
+          return handleError(res, err);
+        }
+        data = [];
+        _.forEach(result, function(row, idx) {
+          return data.push({
+            'browser': row.browser,
+            'percentage': (row.count / length * 100).toFixed(2)
+          });
+        });
+        return res.status(200).json(data);
+      });
+    });
+  };
+
+  exports.country = function(req, res) {
+    return Logger.count(function(err, length) {
+      if (err) {
+        return handleError(res, err);
+      }
+      if (!length) {
+        return res.status(200).end();
+      }
+      return Logger.aggregate([
+        {
+          $group: {
+            _id: {
+              country: '$geoIp.country',
+              id: '$_id'
+            }
+          }
+        }, {
+          $group: {
+            _id: '$_id.country',
+            count: {
+              $sum: 1
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            country: '$_id',
+            count: 1
+          }
+        }
+      ], function(err, result) {
+        var data;
+        if (err) {
+          return handleError(res, err);
+        }
+        data = [];
+        _.forEach(result, function(row, idx) {
+          return data.push({
+            'country': row.country,
+            'percentage': (row.count / length * 100).toFixed(2)
+          });
+        });
+        return res.status(200).json(data);
+      });
+    });
+  };
+
+  exports.platform = function(req, res) {
+    return Logger.count(function(err, length) {
+      if (err) {
+        return handleError(res, err);
+      }
+      if (!length) {
+        return res.status(200).end();
+      }
+      return Logger.aggregate([
+        {
+          $group: {
+            _id: {
+              platform: '$platform',
+              id: '$_id'
+            }
+          }
+        }, {
+          $group: {
+            _id: '$_id.platform',
+            count: {
+              $sum: 1
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            platform: '$_id',
+            count: 1
+          }
+        }
+      ], function(err, result) {
+        var data;
+        if (err) {
+          return handleError(res, err);
+        }
+        data = [];
+        _.forEach(result, function(row, idx) {
+          return data.push({
+            'platform': row.platform,
+            'percentage': (row.count / length * 100).toFixed(2)
+          });
+        });
+        return res.status(200).json(data);
+      });
+    });
+  };
 
   exports.detail = function(req, res) {
     return Logger.count(function(err, length) {
@@ -138,6 +256,271 @@
         return handleError(res, err);
       }
       return res.status(200).json(result);
+    });
+  };
+
+  exports.detail_browser = function(req, res) {
+    return Logger.count({
+      shortenId: req.params.shortenId
+    }, function(err, length) {
+      if (err) {
+        return handleError(res, err);
+      }
+      if (!length) {
+        return res.status(200).end();
+      }
+      return Logger.aggregate([
+        {
+          $match: {
+            shortenId: req.params.shortenId
+          }
+        }, {
+          $group: {
+            _id: {
+              browser: '$browser',
+              id: '$_id'
+            }
+          }
+        }, {
+          $group: {
+            _id: '$_id.browser',
+            count: {
+              $sum: 1
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            browser: '$_id',
+            count: 1
+          }
+        }
+      ], function(err, result) {
+        var data;
+        if (err) {
+          return handleError(res, err);
+        }
+        data = [];
+        _.forEach(result, function(row, idx) {
+          return data.push({
+            'browser': row.browser,
+            'percentage': (row.count / length * 100).toFixed(2)
+          });
+        });
+        return res.status(200).json(data);
+      });
+    });
+  };
+
+  exports.detail_country = function(req, res) {
+    return Logger.count({
+      shortenId: req.params.shortenId
+    }, function(err, length) {
+      if (err) {
+        return handleError(res, err);
+      }
+      if (!length) {
+        return res.status(200).end();
+      }
+      return Logger.aggregate([
+        {
+          $match: {
+            shortenId: req.params.shortenId
+          }
+        }, {
+          $group: {
+            _id: {
+              country: '$geoIp.country',
+              id: '$_id'
+            }
+          }
+        }, {
+          $group: {
+            _id: '$_id.country',
+            count: {
+              $sum: 1
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            country: '$_id',
+            count: 1
+          }
+        }
+      ], function(err, result) {
+        var data;
+        if (err) {
+          return handleError(res, err);
+        }
+        data = [];
+        _.forEach(result, function(row, idx) {
+          return data.push({
+            'country': row.country,
+            'percentage': (row.count / length * 100).toFixed(2)
+          });
+        });
+        return res.status(200).json(data);
+      });
+    });
+  };
+
+  exports.detail_platform = function(req, res) {
+    return Logger.count({
+      shortenId: req.params.shortenId
+    }, function(err, length) {
+      if (err) {
+        return handleError(res, err);
+      }
+      if (!length) {
+        return res.status(200).end();
+      }
+      return Logger.aggregate([
+        {
+          $match: {
+            shortenId: req.params.shortenId
+          }
+        }, {
+          $group: {
+            _id: {
+              platform: '$platform',
+              id: '$_id'
+            }
+          }
+        }, {
+          $group: {
+            _id: '$_id.platform',
+            count: {
+              $sum: 1
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            platform: '$_id',
+            count: 1
+          }
+        }
+      ], function(err, result) {
+        var data;
+        if (err) {
+          return handleError(res, err);
+        }
+        data = [];
+        _.forEach(result, function(row, idx) {
+          return data.push({
+            'platform': row.platform,
+            'percentage': (row.count / length * 100).toFixed(2)
+          });
+        });
+        return res.status(200).json(data);
+      });
+    });
+  };
+
+  exports.detail_referrer = function(req, res) {
+    return Logger.count({
+      shortenId: req.params.shortenId
+    }, function(err, length) {
+      if (err) {
+        return handleError(res, err);
+      }
+      if (!length) {
+        return res.status(200).end();
+      }
+      return Logger.aggregate([
+        {
+          $match: {
+            shortenId: req.params.shortenId
+          }
+        }, {
+          $group: {
+            _id: {
+              referrerHost: '$referrerHost',
+              id: '$_id'
+            }
+          }
+        }, {
+          $group: {
+            _id: '$_id.referrerHost',
+            count: {
+              $sum: 1
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            referrerHost: '$_id',
+            count: 1
+          }
+        }
+      ], function(err, result) {
+        var data;
+        if (err) {
+          return handleError(res, err);
+        }
+        data = [];
+        _.forEach(result, function(row, idx) {
+          return data.push({
+            'referrerHost': row.referrerHost,
+            'percentage': (row.count / length * 100).toFixed(2)
+          });
+        });
+        return res.status(200).json(data);
+      });
+    });
+  };
+
+  exports.detail_remoteip = function(req, res) {
+    return Logger.count({
+      shortenId: req.params.shortenId
+    }, function(err, length) {
+      if (err) {
+        return handleError(res, err);
+      }
+      if (!length) {
+        return res.status(200).end();
+      }
+      return Logger.aggregate([
+        {
+          $match: {
+            shortenId: req.params.shortenId
+          }
+        }, {
+          $group: {
+            _id: {
+              remoteIp: '$remoteIp',
+              id: '$_id'
+            }
+          }
+        }, {
+          $group: {
+            _id: '$_id.remoteIp',
+            count: {
+              $sum: 1
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            remoteIp: '$_id',
+            count: 1
+          }
+        }
+      ], function(err, result) {
+        var data;
+        if (err) {
+          return handleError(res, err);
+        }
+        data = [];
+        _.forEach(result, function(row, idx) {
+          return data.push({
+            'remoteIp': row.remoteIp,
+            'percentage': (row.count / length * 100).toFixed(2)
+          });
+        });
+        return res.status(200).json(data);
+      });
     });
   };
 
--- server/api/history/index.js
+++ server/api/history/index.js
@@ -12,9 +12,25 @@
 
   router.get('/created', controller.created);
 
+  router.get('/browser', controller.browser);
+
+  router.get('/country', controller.country);
+
+  router.get('/platform', controller.platform);
+
   router.get('/:shortenId', controller.detail);
 
   router.get('/:shortenId/clicks', controller.clicks);
+
+  router.get('/:shortenId/browser', controller.detail_browser);
+
+  router.get('/:shortenId/country', controller.detail_country);
+
+  router.get('/:shortenId/platform', controller.detail_platform);
+
+  router.get('/:shortenId/referrer', controller.detail_referrer);
+
+  router.get('/:shortenId/remoteip', controller.detail_remoteip);
 
   module.exports = router;
 
